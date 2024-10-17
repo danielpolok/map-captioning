8bc98e66485bf8bc2b249ef70ce9cf6e6ddc1431
polok <daniel.polok@nokia.com>
2024-10-17 08:55:07 +0200

    rename

--- src/dataset_downloading.py
+++ src/dataset_downloading.py
@@ -1,23 +1,24 @@
 from pathlib import Path
 from box import ConfigBox
+from ruamel.yaml import YAML
 
 from datasets import load_dataset
-from ruamel.yaml import YAML
+
 
 yaml = YAML(typ="safe")
 
 
-def dataset_download():
+def dataset_downloading() -> None:
     params = ConfigBox(yaml.load(open("params.yaml", encoding="utf-8")))
 
-    dataset_name = params["dataset"]
-    split = params["split"]
+    dataset_name = params.dataset_download.path
+    split = params.dataset_download.split
 
-    dataset_dir = Path("data") / "input"
+    dataset_dir = Path("data") / "dataset" / "input"
 
     dataset = load_dataset(path=dataset_name, split=split)
     dataset.save_to_disk(dataset_dir)
 
 
 if __name__ == "__main__":
-    dataset_download()
+    dataset_downloading()
