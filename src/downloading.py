02386c0563d11187f517c26ca335362c9f3ec2e7
polok <daniel.polok@nokia.com>
2024-10-30 19:46:25 +0100

    rename downloading file script

--- src/downloading.py
+++ src/downloading.py
@@ -11,8 +11,8 @@ yaml = YAML(typ="safe")
 def dataset_downloading() -> None:
     params = ConfigBox(yaml.load(open("params.yaml", encoding="utf-8")))
 
-    dataset_name = params.dataset_download.path
-    split = params.dataset_download.split
+    dataset_name = params.downloading.path
+    split = params.downloading.split
 
     dataset_dir = Path("data") / "dataset" / "input"
 
